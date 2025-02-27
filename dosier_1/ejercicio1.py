a0ada917e00586d293a1d5c790825b45c685b5a7
aferlop5 <aferlop5@upv.edu.es>
2025-02-27 19:47:39 +0100

    borrando todo

--- dosier_1/ejercicio1.py
+++ dosier_1/ejercicio1.py
@@ -1,10 +1,11 @@
 import mysql.connector
 
-con=mysql.connector.connect(user="root",password="root")
+con=mysql.connector.connect(user="root",password="Agustife2005")
 cur=con.cursor()
 cur.execute("create database ej1")
 cur.execute("use ej1")
-cur.execute("create table users(title varchar(50), name varchar(32), surname varchar(32), email varchar(32)")
-cur.commit()
+cur.execute("create table users(title varchar(50), name varchar(32), surname varchar(32), email varchar(32))")
+con.commit()
 con.close()
 print("Base de datos creada")
+~                            
